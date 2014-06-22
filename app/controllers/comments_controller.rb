2fe22d085901ff73bed4f3b388cdf1c1326f44c4
catherinev <catherinev@users.noreply.github.com>
2014-06-22 10:57:51 -0500

    fix ability to add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
 	def create
+		@user = User.find(params[:user_id])
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 
-		redirect_to article_path(@article)
+		redirect_to user_article_path(params[:user_id], @article)
 	end
 
 	def destroy
@@ -11,7 +12,7 @@ class CommentsController < ApplicationController
 		@comment = Comment.find(params[:id])
 		@comment.destroy
 
-		redirect_to article_path(@article)
+		redirect_to user_article_path(params[:user_id], @article)
 	end
 
 
